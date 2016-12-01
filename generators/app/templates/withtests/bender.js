5fd8a640687ae9c6f16f9cd5245a8139f18ce956
Alex Tsai <caffodian@gmail.com>
2016-12-01 13:39:25 -0800

    fix some missing templating

--- generators/app/templates/withtests/bender.js
+++ generators/app/templates/withtests/bender.js
@@ -43,7 +43,7 @@ var config = {
     ],
 
     tests: {
-        detabler: {
+        '<%= pluginName %>': {
             applications: ['ckeditor'],
             basePath: 'tests/',
             paths: [
